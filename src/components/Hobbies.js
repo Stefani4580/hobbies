78186f7983620ec4e0691232a12fa71fd8a46e9a
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-11 19:36:43 -0400

    Styling added.  Proud of my new css skills.

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -3,10 +3,13 @@ import React from 'react'
 function Hobbies(props) {
     return (
         <div>
-            <h2>My favorite hobby is</h2>
-            <h3>{props.name}</h3>
-            <img src={props.pic} alt="hobby"/>
-            <p>{props.details}</p>
+            <div className="hobbyCard">
+                <h2 className="name">{props.name}</h2>
+                <div className="picDets">
+                <img className="pic" src={props.pic} alt="hobby"/>
+                <p className="details">{props.details}</p>               
+                </div>
+            </div>
         </div>
     )
 }
